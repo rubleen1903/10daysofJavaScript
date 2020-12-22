499d3fae199a18127f0eb862638baca90513d8e6
Rubleen Kaur <rubleen16@gmail.com>
2020-12-23 02:18:44 +0530

    Create day7regularexpression3.js

---
+++ day7regularexpression3.js
@@ -0,0 +1,19 @@
+
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    
+    
+    /*
+    
+   
+     * Do not remove the return statement
+     */
+    
+     var re = RegExp('\\d+', 'g');
+    return re;
+}
+
